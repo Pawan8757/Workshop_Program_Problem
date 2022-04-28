337638937fa7277c9c463bc3d985c830333d88d5
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-04-28 10:52:57 +0530

    adding temperature conversion problem

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureProblem
+{
+    public class Class1
+    {
+        public  void FarheinheitToCelcius()
+        {
+            Console.WriteLine("Enter the temp");
+            int farhenheit = Convert.ToInt32(Console.ReadLine());
+            double celcius = (farhenheit - 32) * 5 / 9;
+            Console.WriteLine("celcius=" + celcius);
+            Console.ReadLine();
+        }
+
+        public  void CelciusToFarheinheit()
+        {
+            Console.WriteLine("Enter the temp");
+            int celcius = Convert.ToInt32(Console.ReadLine());
+
+            double farheinheit = (celcius * 9 / 5) + 32;
+            Console.WriteLine("farheinheit = " + farheinheit);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TemperatureProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("It's temperature conversion program");
+            
+            Class1 class1 = new Class1();
+            class1.CelciusToFarheinheit();
+           // class1.FarheinheitToCelcius();
+        }
+    }
+}
